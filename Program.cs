0214f0f9503f49891acfe8c5cc4b1623764ba6f7
F_D <den9152@yandex.ru>
2022-11-09 20:43:41 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+   
+    {
+        byte jop = 250;
+        try
+        {
+            student one = new student("Иванов", "Коля", checked((byte) (jop+60)));
+            Console.WriteLine(one);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            throw;
+        }
+      
+
+
+    //    byte c = 200;
+    //    byte w = 100;
+    //    try
+    //    {
+    //        //int d = 4;
+    //        //int y = 0;
+    //        //int p = d / y;
+           
+    //        byte po = (checked((byte)(c + w)));
+    //    }
+    //    catch (Exception yu)
+    //    {
+    //       Console.WriteLine(yu.Message);
+    //        throw;
+    //    }
+    }
+
---
+++ student.cs
@@ -0,0 +1,27 @@
+﻿
+    internal class student
+    {
+        string _name;
+        string _fname;
+        byte _age;
+
+       
+        public student (string name, string fname, byte age)
+        {
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+            this._name = name;
+            if (fname == null)
+                throw new ArgumentNullException(nameof(fname));
+            this._fname = fname;
+            if (age == null)
+                throw new ArgumentNullException(nameof(age));
+            this._age = age;
+        }
+        public override string ToString()
+        {
+            return $"Фамилия{this._fname}, имя {this._name}, возраст {this._age}";
+        }
+
+    }
+
